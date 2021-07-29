ece95129725286d9a6af7439a0082ed13d0bb4c2
Hosanna <hosabivan@gmail.com>
2021-07-29 22:08:50 +0100

    major functionalities

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");   
+});
+
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(1000, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type = 'text'").keypress(function(e){
+    if (e.which === 13) {
+        var newTodo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li>  <span>X</span> " + newTodo + "</li>")
+    }
+});
